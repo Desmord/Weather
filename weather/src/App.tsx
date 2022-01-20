796e2b0dc71070d47e23ca3712c0407c0d6f9578
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2022-01-20 13:51:12 +0100

    Add dark/light mode global css theme

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,18 +1,46 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalTheme } from './GlobalTheme'
 
 // Components
 import Loader from "./Components/Loader/Loader";
 
-const Container = styled.div``
+const Container = styled.div<{ darkMode: boolean }>`
+  width: 100vw;
+  height: 100vh;
+  display:grid;
+  grid-template-columns: 1fr 250px 250px 1fr;
+  grid-template-rows: 100px 400px;
 
+  ${({ theme, darkMode }) => darkMode ?
+    `
+    background-color: ${theme.colors.dark.appBackground};
+    color: ${theme.colors.dark.fontColor};
+    ` : `
+    background-color: ${theme.colors.light.appBackground};
+    color: ${theme.colors.light.fontColor};
+    `}
+
+
+  @media all and (max-width: 540px){
+    grid-template-columns: 1fr 200px 200px 1fr;
+    grid-template-rows: 1fr 350px 1fr;
+  }
+
+`
 
 
 const App = () => {
 
+  const [darkMode, setDarkMode] = useState(true)
+
+
   return (
-    <Container>
-      <Loader />
-    </Container>
+    <ThemeProvider theme={GlobalTheme}>
+      <Container darkMode={darkMode}>
+        <Loader />
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ weather/src/GlobalTheme.ts
@@ -0,0 +1,15 @@
+export const GlobalTheme = {
+    colors: {
+        dark: {
+            appBackground: `rgba(0,0,0,0.9)`,
+            fontColor: `rgba(255,255,255,0.9)`,
+        },
+        light: {
+            appBackground: `rgba(255,255,255,0.9)`,
+            fontColor: `rgba(0,0,0,0.9)`,
+        }
+    }
+
+}
+
+export default GlobalTheme
